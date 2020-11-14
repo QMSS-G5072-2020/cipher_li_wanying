cd88d528953f9d683a531b8558079b716cca316a
Wanyingli <wl2722@columbia.edu>
2020-11-13 20:13:37 -0500

    initial package setup

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use cipher_wl2722 in a project::
+
+    import cipher_wl2722
---
+++ tests/test_cipher_wl2722.py
@@ -0,0 +1,5 @@
+from cipher_wl2722 import __version__
+from cipher_wl2722 import cipher_wl2722
+
+def test_version():
+    assert __version__ == '0.1.0'
